3813bfd6a275c4bf659ed2f7fa29ce52958a529b
Irina Dolgova <claycharm@yandex.ru>
2022-05-18 17:54:00 +0300

    add src

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,2 +1,63 @@
-package ru.netology.stats;public class StatsService {
+package ru.netology.stats;
+
+public class StatsService {
+    public int calculateSum(int[] salesOfMonth) {
+        int sum = 0;
+        for (int eachSalesMonth : salesOfMonth) {
+            sum += eachSalesMonth;
+        }
+        return sum;
+    }
+
+    public int calculateAvg(int[] salesOfMonth) {
+        return calculateSum(salesOfMonth) / salesOfMonth.length;
+    }
+
+    public int findMax(int[] salesOfMonth) {
+        int currentMax = 0;
+        int month = 0;
+        for (int eachSalesMonth : salesOfMonth) {
+            if (eachSalesMonth >= salesOfMonth[currentMax]) {
+                currentMax = month;
+            }
+            month = month + 1;
+        }
+        return currentMax + 1;
+    }
+
+    public int findMin(int[] salesOfMonth) {
+        int currentMin = 0;
+        int month = 0;
+        for (int eachSalesMonth : salesOfMonth) {
+            if (eachSalesMonth <= salesOfMonth[currentMin]) {
+                currentMin = month;
+            }
+            month = month + 1;
+        }
+        return currentMin + 1;
+    }
+
+    public int belowAvg(int[] salesOfMonth) {
+        int currentBelowAvg = 0;
+        int avg = calculateSum(salesOfMonth) / salesOfMonth.length;
+        for (int eachSalesMonth : salesOfMonth) {
+            if (eachSalesMonth < avg) {
+                currentBelowAvg++;
+            }
+            ;
+        }
+        return currentBelowAvg++;
+    }
+
+    public int overAvg(int[] salesOfMonth) {
+        int currentOverAvg = 0;
+        int avg = calculateSum(salesOfMonth) / salesOfMonth.length;
+        for (int eachSalesMonth : salesOfMonth) {
+            if (eachSalesMonth > avg) {
+                currentOverAvg++;
+            }
+            ;
+        }
+        return currentOverAvg++;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,4 +1,60 @@
+package ru.netology.stats;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StatsServiceTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSum(salesOfMonth);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateAvg() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calculateAvg(salesOfMonth);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindMax() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.findMax(salesOfMonth);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindMin() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.findMin(salesOfMonth);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldBelowAvg() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.belowAvg(salesOfMonth);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldOverAvg() {
+        StatsService service = new StatsService();
+        int[] salesOfMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.overAvg(salesOfMonth);
+        assertEquals(expected, actual);
+    }
 }
